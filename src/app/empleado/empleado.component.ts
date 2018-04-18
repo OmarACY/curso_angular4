fe43331074ad5d7463f439a0ff500e4449c39e09
Omar Alejandro Cervantes Yepes <oacervantes@cor.potosi.com.mx>
2018-04-18 10:49:45 -0500

    Seccion 4 - Clase 21

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -7,12 +7,45 @@ import { Component } from '@angular/core';
 
 export class EmpleadoComponent {
     tituloComponente = 'Empleado';
-    nombre:string = 'Omar Alejandro Cervantes Yepez';
-    edad:number = 24;
+    nombre:string;
+    edad:number;
     mayorDeEdad:boolean = true;
-    trabajos:Array<any> = [5, ' Programador analista', ' Levantamiento de tarros XD'];
+    trabajos:Array<any>;
+    comodin:any;
 
     constructor(){
         console.log(this.trabajos);
+        this.nombre = 'Omar Alejandro Cervantes Yepez';
+        this.edad = 24;
+        this.mayorDeEdad = true;
+        this.trabajos = [5, ' Programador analista', ' Levantamiento de tarros XD'];
+        this.comodin = 'SI';
+    }
+
+    ngOnInit(){
+        this.cambiarNombre('Juan Perez');
+        this.cambiarEdad(22);
+        //alert('Bienvenido!!' + this.nombre + ' Con edad ' + this.edad);
+    
+        // Variables y alcance
+        var uno = 8;
+        var dos = 15;
+
+        if(uno === 8){
+            let uno = 3;
+            var dos = 88;
+
+            console.log("Dentro de if: " + uno + " " + dos);
+        }
+
+        console.log("Fuera de de if: " + uno + " " + dos);
+    }
+
+    cambiarNombre(nombre){
+        this.nombre = nombre;
+    }
+
+    cambiarEdad(edad){
+        this.edad = edad;
     }
 }
