ca1f7d7154d7aab224a2768ef1136b9a983a9316
Omar Alejandro Cervantes Yepes <oacervantes@cor.potosi.com.mx>
2018-04-19 12:40:00 -0500

    Sección 7 - Clase 36 Crear y utilizar un servicio

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { RopaService } from '../services/ropa.service';
 
 @Component({
     selector:'home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    providers: [RopaService]
 })
 
 export class HomeComponent{
     public titulo = "Página principal";
+
+    constructor(
+        private _ropaService:RopaService
+    ){}
+
+    ngOnInit(){
+        console.log(this._ropaService.prueba('Camiseta nike'));
+    }
 }
