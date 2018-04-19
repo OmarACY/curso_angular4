beefaa841dbf1cfe3dc7d8f296a8e2b5c6de07e8
Omar Alejandro Cervantes Yepes <oacervantes@cor.potosi.com.mx>
2018-04-19 18:24:56 -0500

    Sección 9, clase 45 Ejercicio con formularios. Primera parte.

--- src/app/coche/coche.component.ts
+++ src/app/coche/coche.component.ts
@@ -9,13 +9,19 @@ import { Coche } from './coche';
 export class CocheComponent{
     public titulo:string;
     public coche: Coche;
+    public coches:Array<Coche>;
 
     constructor(){
         this.titulo = 'Componente de coche';
         this.coche = new Coche("", "", "");
+        this.coches = [
+            new Coche("Seat Panda", "120", "Blanco"),
+            new Coche("Renault Clio", "110", "Azul")
+        ]
     }
 
     onSubmit(){
-        console.log(this.coche);
+        this.coches.push(this.coche);
+        this.coche = new Coche("", "", "");
     }
 }
