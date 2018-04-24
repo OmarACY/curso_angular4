2e74603b9187360c6838a375576af9be178d9279
Omar Alejandro Cervantes Yepes <oacervantes@cor.potosi.com.mx>
2018-04-24 17:35:38 -0500

    Sección 12, clase 65  Método para guardar productos

--- curso-angular4-backend/index.php
+++ curso-angular4-backend/index.php
@@ -14,5 +14,47 @@
         echo "Texto cualquiera";
     });
 
+    $app->post('/producto', function() use($app, $db){
+        $json = $app->request->post('json');
+        $data = json_decode($json, true);
+        
+        if(!isset($data['nombre'])){
+            $data['nombre'] = null;
+        }
+
+        if(!isset($data['descripcion'])){
+            $data['descripcion'] = null;
+        }
+
+        if(!isset($data['precio'])){
+            $data['precio'] = null;
+        }
+
+        if(!isset($data['rutaImagen'])){
+            $data['rutaImagen'] = null;
+        }
+
+        $query = "INSERT INTO productos (id, nombre, descripcion, precio, rutaImagen) ".
+                 " VALUES(NULL,'{$data['nombre']}', '{$data['descripcion']}', {$data['precio']}, '{$data['rutaImagen']}');";
+    
+        $insert = $db->query($query);
+
+        if($insert){
+            $result = array(
+                'status' => 'success',
+                'code'   => 200,
+                'message'=> 'Producto creado correctamente' 
+            );
+        }else{
+            $result = array(
+                'status' => 'error',
+                'code'   => 404,
+                'message'=> 'Producto no creado' 
+            );
+        }
+
+        echo json_encode($result);
+    });
+
     $app->run();
 ?>
